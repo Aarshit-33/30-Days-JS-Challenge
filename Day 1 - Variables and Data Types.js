a3dd038539f588d9f4e3369f1eebd583948ca525
Aarshit Jolapara <aarshitjolapara11@gmail.com>
2024-07-13 22:10:10 +0530

    first day variable and data types

---
+++ Day 1 - Variables and Data Types.js
@@ -0,0 +1,53 @@
+// Activity 1 :: ----------------------------------------------------------
+
+// Task 1 ::
+var num = 5;
+console.log(num); // 5
+
+// Task 2 ::
+let str = "XYZ";
+console.log(str); // XYZ
+
+// Activity 2 :: ----------------------------------------------------------
+
+// Task 3 ::
+const flag = false;
+console.log(flag); // false
+
+// Activity 3 :: ----------------------------------------------------------
+
+// Task 4 ::
+let varInt = 1;
+let varFlt = 1.5;
+let varBool = true;
+let varObj = {
+	name: "ABC",
+	age: 5,
+};
+let varArr = [5, "XYZ", 5.6, true];
+let varStr = "HELLO";
+
+console.log(varInt + " is a " + typeof varInt + " type of variable");
+console.log(varFlt + " is a " + typeof varFlt + " type of variable");
+console.log(varStr + " is a " + typeof varStr + " type of variable");
+console.log(varBool + " is a " + typeof varBool + " type of variable");
+console.log(varObj + " is a " + typeof varObj + " type of variable");
+console.log(varArr + " is a " + typeof varArr + " type of variable");
+
+// Activity 4 :: ----------------------------------------------------------
+
+// Task 5 ::
+
+let test = "XYZ";
+console.log("Initial value :: " + test); // XYZ
+test = "ABC";
+console.log("Reassigned value :: " + test); // ABC
+
+// Activity 5 :: ----------------------------------------------------------
+
+// Task 6 ::
+``;
+const nStr = "Hello World";
+console.log("Initial value :: " + nStr); // Hello World
+// nStr = "Good Morning";         // Error (TypeError - Assigning value to const type of variable)
+// console.log("Reassigned value :: " + nStr);
